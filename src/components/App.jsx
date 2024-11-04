dd27c4acc8394cf5ed52d9f70e55851622786795
galynary <107271972+galynary@users.noreply.github.com>
2024-11-04 16:30:22 +0200

    CHANGE FILE

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,47 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Header from './Header/Header';
+//import Counter from './Counter/Counter';
+import FormLogin from './formLogin/FormLogin';
+import Modal from './Modal/Modal';
+import ToDoList from './TodoList/TodoList';
+import { nanoid } from 'nanoid';
+
+class App extends Component {
+  state = {
+    isShowModal: false,
+  };
+
+  showModal = () => {
+    this.setState({ isShowModal: true });
+  };
+
+  closeModal = () => {
+    this.setState({ isShowModal: false });
+  };
+  createUser = data => {
+    const newUser = {
+      ...data,
+      id: nanoid(),
+    };
+    console.log(newUser);
+  };
+
+  render() {
+    return (
+      <div className="container-todo">
+        {/* Pass the showModal function to Header */}
+        <Header showModal={this.showModal} />
+        {/*<Counter />*/}
+
+        <ToDoList />
+        {this.state.isShowModal && (
+          <Modal closeModal={this.closeModal}>
+            <FormLogin createUser={this.createUser} />
+          </Modal>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
